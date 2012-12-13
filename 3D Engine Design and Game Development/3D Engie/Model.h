ecdce7e79e4636e73615ca9d427dc44a0260028f
Kevin van Leeuwen <kevin_rsx@msn.com>
2012-12-13 14:04:36 +0100

    model en texture toegevoegd

---
+++ 3D Engine Design and Game Development/3D Engie/Model.h
@@ -0,0 +1,10 @@
+#include <string>
+#include <d3dx9.h>
+
+class Model
+{
+public:
+	Model();
+	~Model();
+};
+
---
+++ 3D Engine Design and Game Development/3D Engie/Texture.cpp
@@ -0,0 +1,31 @@
+#include "Texture.h"
+
+
+Texture::Texture(std::string& argName, LPDIRECT3DTEXTURE9 texture)
+{
+	name = argName;
+	loadedTexture = texture;
+}
+
+
+Texture::~Texture()
+{
+	loadedTexture->Release();
+}
+
+std::string Texture::GetName()
+{
+	return name;
+}
+
+void* Texture::getTexture()
+{
+	return loadedTexture;
+}
+
+void Texture::setTextureObject(void* texture)
+{
+	loadedTexture = (LPDIRECT3DTEXTURE9)texture;
+}
+
+
