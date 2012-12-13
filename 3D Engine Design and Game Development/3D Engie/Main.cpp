01063b4d0b46911ef9712a562b601fe1a82f228b
Casper <casper.rooker@gmail.com>
2012-12-13 14:02:36 +0100

    rest van die shit

--- 3D Engine Design and Game Development/3D Engie/Main.cpp
+++ 3D Engine Design and Game Development/3D Engie/Main.cpp
@@ -1,12 +1,14 @@
 #include "Logger.h"
 #include "Window.h"
 #include "WindowManager.h"
+#include "RendererDX9.h"
 
 int main()
 {
 	WindowManager* wm = new WindowManager();
 	wm->AddWindow("Window test", 250, 150, 400, 400);
-	wm->GetWindow("Window test")->Show();
+	wm->GetWindow("Window test")->Show(SW_NORMAL);
 	
+
 	return 0;
 };
