3088c965798a8ed128796bd72150624955806577
Tom van den Bulk <woaortom@gmail.com>
2013-01-20 21:39:31 +0100

    scene promo

--- 3D Engine Design and Game Development/3D Engie/Scene.cpp
+++ 3D Engine Design and Game Development/3D Engie/Scene.cpp
@@ -12,7 +12,41 @@ Scene::~Scene()
 
 void Scene::LoadScene(std::string sceneName)
 {
+	Logger::GetInstance()->Write("Loading scene " + sceneName);
+	
+	std::ifstream fileStream(sceneName);
 
+	if(fileStream.good())
+	{
+		while(!fileStream.eof())
+		{
+			std::string line;
+			std::getline(fileStream, line);
+
+			if(line.compare("Camera") != 0)
+			{
+				//maak camera aan
+			}
+			else if(line.compare("Sky") != 0)
+			{
+				//maak sky aan
+			}
+			else if(line.compare("Terrain") != 0)
+			{
+				//maak terrain aan
+			}
+			else if(line.compare("Entity") != 0)
+			{
+				//maak entity aan
+			}
+			std::cout << line << std::endl;
+		}
+		
+	}
+	else
+	{
+		Logger::GetInstance()->Write("can't open scene file: " + sceneName);
+	}
 }
 
 void Scene::RenderScene()
